26946ae9ea57580efb9f11eb06b137d19f9c96d7
Future Computer Training Institute <107338637+fctiadmin@users.noreply.github.com>
2024-10-19 03:24:53 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<?php 
+		$catagory=array("CPU"=>"Cori3","Ram"=>"DDR4 8GB","HDD"=>"500GB");
+	?>
+	<select name="" id="">
+			<?php
+			foreach($catagory as $element=>$value){?>
+			<option value="<?=$value?>"><?=$value.'('.$element.')'?></option>
+			<?php
+				
+			}
+			?>
+	</select>
+</body>
+</html>
